b3365429568af1705587252e519672bea09ff372
Rymar Anastasia <ravsec@yandex.ru>
2026-06-27 02:31:55 +0700

    first commit

---
+++ src/main/java/methods/RequestSpec.java
@@ -0,0 +1,19 @@
+package methods;
+
+import constants.ApiConstants;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+
+public class RequestSpec {
+
+    // Метод для создания базовой спецификации запроса
+    public static RequestSpecification requestSpecification() {
+        return given().log().all() // Начинаем создание запроса и логируем все действия
+                .contentType(ContentType.JSON) // Устанавливаем тип контента как JSON
+                .baseUri(ApiConstants.BURGERS_URL); // Устанавливаем базовый URL из ApiConstants
+    }
+
+}
---
+++ src/test/java/CreateUserTest.java
@@ -0,0 +1,124 @@
+import methods.RequestSpec;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.github.javafaker.Faker;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import models.User;
+import methods.UserRequests;
+
+import static constants.ApiConstants.BURGERS_URL;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CreateUserTest {
+    private User user;
+    private UserRequests userRequests;
+    private String accessToken;
+    private Faker faker;
+
+    @Before
+    public void setUp() {
+        RestAssured.requestSpecification = RequestSpec.requestSpecification();
+        userRequests = new UserRequests();
+        faker = new Faker();
+    }
+
+    @Test
+    @DisplayName("Создание уникального пользователя")
+    public void createUniqueUser() {
+        user = new User(
+                faker.internet().emailAddress(),
+                faker.internet().password(6, 10),
+                faker.name().firstName()
+        );
+
+        Response responseCreate = userRequests.createUser(user);
+
+        responseCreate.then().log().all()
+                .statusCode(200)
+                .body("success", equalTo(true));
+
+        accessToken = responseCreate.jsonPath().getString("accessToken");
+    }
+
+    @Test
+    @DisplayName("Создание пользователя, который уже зарегистрирован")
+    public void createExistingUser() {
+        user = new User(
+                faker.internet().emailAddress(),
+                faker.internet().password(6, 10),
+                faker.name().firstName()
+        );
+
+        Response firstRegister = userRequests.createUser(user);
+        firstRegister.then().statusCode(200);
+        accessToken = firstRegister.jsonPath().getString("accessToken");
+
+        Response secondRegister = userRequests.createUser(user);
+
+        secondRegister.then().log().all()
+                .statusCode(403)
+                .body("success", equalTo(false))
+                .body("message", equalTo("User already exists"));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя без email")
+    public void createUserWithoutEmail() {
+        user = new User(
+                null,
+                faker.internet().password(6, 10),
+                faker.name().firstName()
+        );
+
+        Response responseCreate = userRequests.createUser(user);
+
+        responseCreate.then().log().all()
+                .statusCode(403)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя без password")
+    public void createUserWithoutPassword() {
+        user = new User(
+                faker.internet().emailAddress(),
+                null,
+                faker.name().firstName()
+        );
+
+        Response responseCreate = userRequests.createUser(user);
+
+        responseCreate.then().log().all()
+                .statusCode(403)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя без name")
+    public void createUserWithoutName() {
+        user = new User(
+                faker.internet().emailAddress(),
+                faker.internet().password(6, 10),
+                null
+        );
+
+        Response responseCreate = userRequests.createUser(user);
+
+        responseCreate.then().log().all()
+                .statusCode(403)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+    @After
+    public void deleteUser() {
+        if (accessToken != null && !accessToken.isEmpty()) {
+            userRequests.deleteUser(accessToken);
+        }
+    }
+}
